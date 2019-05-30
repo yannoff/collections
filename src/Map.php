c9547007d0ba85f6d70af3582095af1c68e09a4c
Yannoff <yannoff.it@gmail.com>
2019-05-30 21:26:33 +0200

    Add legal notice

--- src/Map.php
+++ src/Map.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the yannoff/collections library
+ * (c) 2019 Yannoff (https://github.com/yannoff)
+ *
+ * @project   yannoff/collections
+ * @link      https://github.com/yannoff/collections
+ * @license   http://opensource.org/licenses/MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Yannoff\Component\Collections;
 
